a0cb16fad7895313ea62313cd0ef46787d56f76f
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-01-20 18:22:30 +0100

    feat: add colored ASCII art and enhance configuration initialization

--- src/init.rs
+++ src/init.rs
@@ -1,8 +1,11 @@
-use crate::template;
+use crate::{template, utils};
 use std::{fs::File, io::Write};
 
 pub fn init_config() {
+    utils::rustyspider_ascii_art();
+    println!("Initialize RustySpider configuration file...");
     let mut f = File::create("rustyspider_config.json").expect("Unable to create file");
     f.write_all(&template::config_file_template().as_bytes())
         .expect("Unable to write data");
+    println!("Configuration file initialize successfully!");
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,7 @@
 use std::process::exit;
 
+use colored::Colorize;
+
 pub fn rustyspider_usage() {
     let usage = r"
 Usage: rustyspider command [options]
@@ -22,4 +24,16 @@ Options:
 pub fn command_usage(usage: &str) {
     println!("{}", usage);
     exit(0);
+}
+
+pub fn rustyspider_ascii_art() {
+    let ascii = r"
+__________                 __             _________        .__     .___              
+\______   \ __ __  _______/  |_  ___.__. /   _____/______  |__|  __| _/ ____ _______ 
+ |       _/|  |  \/  ___/\   __\<   |  | \_____  \ \____ \ |  | / __ |_/ __ \\_  __ \
+ |    |   \|  |  /\___ \  |  |   \___  | /        \|  |_> >|  |/ /_/ |\  ___/ |  | \/
+ |____|_  /|____//____  > |__|   / ____|/_______  /|   __/ |__|\____ | \___  >|__|   
+        \/            \/         \/             \/ |__|             \/     \/                                                                             
+  ";
+    println!("{}", ascii.truecolor(255, 94, 0))
 }
