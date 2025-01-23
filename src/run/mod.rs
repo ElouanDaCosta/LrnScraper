9fc39c64499f081f4fdca7a5b8f93988983ec8eb
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-01-23 11:06:00 +0100

    feat: add save_file field to WebConfig and implement multi-threaded HTML downloading with file saving

--- src/run/mod.rs
+++ src/run/mod.rs
@@ -19,6 +19,7 @@ pub struct WebConfigData {
 pub struct WebConfig {
     id: String,
     name: String,
+    save_file: String,
     urls: Vec<String>,
 }
 
@@ -26,13 +27,15 @@ pub fn run_scrapper() {
     log::info_log("Getting the config file content...".to_string());
     let websites = get_config_file_content();
     log::info_log("Start scraping process...".to_string());
-    std::thread::scope(|scope| {
+    std::thread::scope(|_scope| {
         let mut threads: Vec<thread::JoinHandle<()>> = Vec::new();
+        let mut save_file: Vec<String> = Vec::new();
         for website in websites.clone() {
+            let save_file_clone = website.save_file.clone();
             let thread = std::thread::spawn(move || download_website(&website));
+            save_file.push(save_file_clone);
             threads.push(thread);
         }
-
         for thread in threads {
             thread.join().unwrap();
         }
@@ -49,6 +52,7 @@ fn get_config_file_content() -> Vec<WebConfig> {
         websites.push(WebConfig {
             id: website.id.clone(),
             name: website.name.clone(),
+            save_file: website.save_file.clone(),
             urls: website.urls.clone(),
         });
     }
@@ -58,23 +62,38 @@ fn get_config_file_content() -> Vec<WebConfig> {
 // create a new thread that scrape the html from given url
 fn download_website(website: &WebConfig) {
     println!("Thread started for id: {}", website.id);
+    let mut threads: Vec<thread::JoinHandle<()>> = Vec::new();
     for url in &website.urls {
         // let response = reqwest::blocking::get(url);
         // let html_from_request = response.unwrap().text().unwrap();
-        let html_from_browser = browser::browse_website(&url);
-        if html_from_browser.is_err() {
-            log::error_log(html_from_browser.as_ref().unwrap_err().to_string());
-        }
-        let parser = parse_html_content(html_from_browser.unwrap(), "title".to_string());
-        if parser.is_empty() {
-            log::error_log_with_code(
-                "Error getting the content from id:".to_string(),
-                website.id.to_string(),
-            );
-        }
-        save_html_content(parser, "test_file");
+        let id_clone = website.id.clone();
+        let save_file_clone = website.save_file.clone();
+        let url_clone = url.clone();
+        let thread =
+            std::thread::spawn(move || sub_thread_url(&url_clone, id_clone, save_file_clone));
+        threads.push(thread);
+    }
+    for thread in threads {
+        thread.join().unwrap();
     }
     println!("Thread finished for id: {}", website.id);
+}
+
+fn sub_thread_url(url: &str, id: String, save_file: String) {
+    println!("Sub-Thread started for id: {}", id);
+    let html_from_browser = browser::browse_website(&url);
+    if html_from_browser.is_err() {
+        log::error_log(html_from_browser.as_ref().unwrap_err().to_string());
+    }
+    let parser = parse_html_content(html_from_browser.unwrap(), "title".to_string());
+    if parser.is_empty() {
+        log::error_log_with_code(
+            "Error getting the content from id:".to_string(),
+            id.to_string(),
+        );
+    }
+    save_html_content(parser, &save_file);
+    println!("Sub-Thread finished for id: {}", id);
 }
 
 // parse the given html document
--- src/run/save_content.rs
+++ src/run/save_content.rs
@@ -1,7 +1,14 @@
-use std::fs;
+use std::{fs::OpenOptions, io::Write};
 
 pub fn save_html_content(data: Vec<String>, filename: &str) {
     for i in data {
-        fs::write(filename, i).expect("Unable to write file");
+        let line_break = i + "\n";
+        let mut f = OpenOptions::new()
+            .append(true)
+            .create(true) // Optionally create the file if it doesn't already exist
+            .open(filename)
+            .expect("Unable to open file");
+        f.write_all(line_break.as_bytes())
+            .expect("Unable to write data");
     }
 }
