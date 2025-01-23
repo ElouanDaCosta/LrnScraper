dbb84bebadaf329566db2465a92bb6a116e711c3
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-01-23 23:04:03 +0100

    refactor: rename sub_thread_url to parse_and_save_content and update documentation for clarity

--- src/run/mod.rs
+++ src/run/mod.rs
@@ -37,15 +37,21 @@ pub fn run_scrapper() {
         pool.queue_work(Box::new(move || download_website(&website)));
     }
 }
-
-// create a new thread that scrape the html from given url
+/// The `download_website` function in Rust downloads multiple URLs concurrently and prints the time
+/// taken for each website.
+///
+/// Arguments:
+///
+/// * `website`: The `download_website` function takes a reference to a `WebConfig` struct as a
+/// parameter. The `WebConfig` struct likely contains information about a website to be downloaded, such
+/// as its ID and a list of URLs to download. The function iterates over each URL in the `urls`
 fn download_website(website: &WebConfig) {
     let start = Instant::now();
     println!("Thread started for id: {}", website.id);
     for url in &website.urls {
         let save_file_clone = website.save_file.clone();
         let url_clone = url.clone();
-        sub_thread_url(&url_clone, save_file_clone);
+        parse_and_save_content(&url_clone, save_file_clone);
     }
     let duration = start.elapsed().as_secs_f32();
     println!(
@@ -54,7 +60,7 @@ fn download_website(website: &WebConfig) {
     );
 }
 
-fn sub_thread_url(url: &str, save_file: String) {
+fn parse_and_save_content(url: &str, save_file: String) {
     let html_from_browser = browser::browse_website(&url);
     if html_from_browser.is_err() {
         log::error_log(html_from_browser.as_ref().unwrap_err().to_string());
@@ -68,21 +74,6 @@ fn sub_thread_url(url: &str, save_file: String) {
     }
     save_html_content(parser, &save_file);
 }
-
-// fn crawl_url(url: &str, save_file: String) {
-//     let html_from_browser = browser::browse_website(&url);
-//     if html_from_browser.is_err() {
-//         log::error_log(html_from_browser.as_ref().unwrap_err().to_string());
-//     }
-//     let parser = parse_html_content(html_from_browser.unwrap(), "a".to_string());
-//     if parser.is_empty() {
-//         log::error_log_with_code(
-//             "Error getting the content for url:".to_string(),
-//             url.to_string(),
-//         );
-//     }
-//     save_html_content(parser, &save_file);
-// }
 
 // parse the given html document
 fn parse_html_content(data: String, tag_selector: String) -> Vec<String> {
